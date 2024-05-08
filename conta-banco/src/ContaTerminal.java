57eab4596ed04a5247ae41d30c4a68822fe36aa3
Bianca Silva <biancaferreiras1997@gmail.com>
2024-05-08 16:51:26 -0300

    feat: Atualizar informações da conta no ContaTerminal.java
    Atualize as informações da conta no arquivo ContaTerminal.java para refletir corretamente o número da agência, o número da conta e o saldo. Além disso, adicione uma mensagem de conta criada para informar ao cliente que a conta foi criada com sucesso.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,26 +6,26 @@ public class ContaTerminal {
 
         // Solicitar informações do usuários
         System.out.print("Por favor, digite o número da Agência: ");
-        String agencia = scanner.nextLine();//0001
+        String agencia = scanner.nextLine();//001
 
         System.out.print("Por favor, digite o nome do cliente: ");
         String nomeCliente = scanner.nextLine();//Caique Brandão
 
         System.out.print("Por favor, digite o número da conta: ");
-        int numero = scanner.nextInt();//1994970
+        int numero = scanner.nextInt();//1021
 
         System.out.print("Por favor, digite o saldo da conta: ");
         double saldo = scanner.nextDouble();//1000.00
-         
-        scanner.close();
 
-          // Exibir informações da conta
-        
+        // Exibir mensagem conta criada
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
         System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo é R$" + saldo + ".");
         System.out.println("Seu saldo já está disponível para saque.");
-    
-    
 
+        
+
+        scanner.close(); // Fechar o scanner
+    
+        
     }
 }
