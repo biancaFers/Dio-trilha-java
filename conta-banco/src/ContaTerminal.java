d774311a07db87584a5b2b062ee035072f357f0d
Bianca Silva <biancaferreiras1997@gmail.com>
2024-05-08 11:33:39 -0300

    meu projeto conta banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+        // Solicitar informações do usuários
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();//0001
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();//Caique Brandão
+
+        System.out.print("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();//1994970
+
+        System.out.print("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();//1000.00
+         
+        scanner.close();
+
+          // Exibir informações da conta
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo é R$" + saldo + ".");
+        System.out.println("Seu saldo já está disponível para saque.");
+    
+    
+
+    }
+}
